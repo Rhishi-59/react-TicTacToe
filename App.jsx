5b4392bfc2d9d42450aba47f0a2b8dd05cd829d0
Rhishi-59 <nairrhishi5@gmail.com>
2024-12-29 21:31:03 +0530

    Add files via upload
    Only components of ticTacToe are added. Setup basic react vite project yourself.

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './components/Form/Form'
+import Card from './components/Card/Card'
+import Grid from './components/Grid/Grid'
+
+function App() {
+
+  return (
+    <>
+    <Grid numberOfCards={9} />     
+      
+    </>
+  )
+}
+
+export default App
